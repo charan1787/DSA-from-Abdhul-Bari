8b9532527f0cd6d71ddcb0e03464d96f5963e168
charan <charan.shankar06@gmail.com>
2024-08-22 23:17:17 +0100

    Count , Max , Sum LL

--- LL.cpp
+++ LL.cpp
@@ -18,7 +18,6 @@ while(q){
 
 void create(int a[],int size){
 struct Node *p=new Node;
-
 for(int i=0;i<size;i++)
 {
     if(i==0){
@@ -40,12 +39,65 @@ for(int i=0;i<size;i++)
 
 }
 
+void Rdisplay(struct Node *first){
+
+    struct Node *p=first;
+    if(p->next==NULL){
+        cout<<p->data;
+        return;
+    }
+    else{
+        cout<<p->data;
+        Rdisplay(p->next);
+        return;
+    }
+}
+
+void count_nodes(struct Node *first){
+    int c=0;
+    struct Node *p = first;
+    while(p){
+        c++;
+        p=p->next;
+    }
+    cout<<"Number of Nodes : "<<c;
+}
+
+void sum_nodes(struct Node *first){
+    int sum=0;
+    struct Node *p = first;
+    while(p){
+        sum=sum+p->data;
+        p=p->next;
+    }
+    cout<<"Sum of Nodes : "<<sum;
+}
+
+void max_node(struct Node *first){
+    struct Node *p=first;
+    int max=INT_MIN;
+
+    while(p){
+        if(p->data>max){
+            max=p->data;
+            p=p->next;
+        }
+        else p=p->next;
+    }
+
+    cout<<"Max Element : "<<max;
+}
 int main()
 {
 int arr[]={5,6,7,8,9};
 int size=sizeof(arr)/sizeof(arr[0]);
     create(arr,size);
-    display(first);
-
+    Rdisplay(first);
+    cout<<endl;
+    count_nodes(first);
+    cout<<endl;
+    sum_nodes(first);
+    cout<<endl;
+    max_node(first);
     return 0;
 }
